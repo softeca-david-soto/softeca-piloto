91b1f691a17667a0dcf4d26789bcf09091379c6e
David Soto <david.soto@softeca.es>
2026-03-10 16:31:27 +0100

    feat: added seeders with roles and perms
    Añado seeders para tema de roles y permisos con spatie y soluciono problema anterior indicando al modelo vendedor su tabla en BBDD

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(PermisosSeeder::class);
+        $this->call(RoleSeeder::class);
         $this->call(ProvinciasSeeder::class);
         $this->call(VendedoresSeeder::class);
         $this->call(ProductosSeeder::class);
---
+++ database/seeders/PermisosSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
+
+class PermisosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $permisos = [
+            'VER_VENDEDORES',
+            'CREAR_VENDEDORES',
+            'EDITAR_VENDEDORES',
+            'BORRAR_VENDEDORES',
+            'VER_TODOS_CLIENTES',
+            'VER_CLIENTES',
+            'CREAR_CLIENTES',
+            'EDITAR_CLIENTES',
+            'BORRAR_CLIENTES',
+            'CREAR_PRODUCTOS',
+            'VER_PRODUCTOS',
+            'EDITAR_PRODUCTOS',
+            'BORRAR_PRODUCTOS',
+        ];
+
+        foreach ($permisos as $perm) {
+            Permission::create([
+                'name' => $perm,
+            ]);
+        }
+    }
+}
