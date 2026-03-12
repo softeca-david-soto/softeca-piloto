6a4dd3085e5d2c36a2ff34d0a0cefb33157197e6
David Soto <david.soto@softeca.es>
2026-03-12 12:08:42 +0100

    feat: add sweetalerts 2 initial config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,11 @@
 <x-layouts::app.sidebar :title="$title ?? null">
     <flux:main>
         {{ $slot }}
+
+		@if (session('swal'))
+			<script>
+				Swal.fire(@json(session('swal')));
+			</script>
+		@endif
     </flux:main>
 </x-layouts::app.sidebar>
