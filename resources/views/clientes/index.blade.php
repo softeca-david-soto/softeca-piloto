99877e1c02a8be8a7ab047d7ba2c38f4bc86b1f2
David Soto <david.soto@softeca.es>
2026-03-13 11:14:16 +0100

    feat: create clientes index view

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -1,1 +1,152 @@
-{{-- Hacer un, si clientes igual a 0 mostrar: No hay clientes asociados a tu usuario --}}
+@if (count($clientes) == 0)
+<x-layouts::app title="Clientes">
+
+    <flux:breadcrumbs >
+        <flux:breadcrumbs.item href="{{ route('dashboard') }}">Dashboard</flux:breadcrumbs.item>
+        <flux:breadcrumbs.item>Mis Clientes</flux:breadcrumbs.item>
+    </flux:breadcrumbs>
+
+    <h1 class="mt-85 text-4xl font-bold tracking-tight text-heading md:text-5xl lg:text-6xl">No hay clientes asociados a tu usuario</h1>
+
+</x-layouts::app>
+@else
+<x-layouts::app title="Clientes">
+	<div class="mb-8 flex justify-between items-center">
+		<flux:breadcrumbs >
+			<flux:breadcrumbs.item href="{{ route('dashboard') }}">Dashboard</flux:breadcrumbs.item>
+			@if ($todos)
+            <flux:breadcrumbs.item>Todos Clientes</flux:breadcrumbs.item>
+            @else
+            <flux:breadcrumbs.item>Mis Clientes</flux:breadcrumbs.item>
+            @endif
+		</flux:breadcrumbs>
+
+        <div class="flex justify-end">
+            <a href="{{ route('clientes.create') }}">
+                <div class="flex gap-2 mb-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                    <span>
+                        New Client
+                    </span>
+
+                </div>
+            </a>
+        </div>
+	</div>
+    @if ($todos)
+    <div>
+        {{-- Nota: aquí puedo añadir un sistema de filtros por comercial, etc. NO ES URGENTE --}}
+    </div>
+    @endif
+
+	<div class="relative overflow-x-auto bg-neutral-primary-soft shadow-xs rounded-base border border-default mb-4">
+    <table class="w-full text-sm text-left rtl:text-right text-body">
+	<thead class="text-sm text-body bg-neutral-secondary-soft border-b rounded-base border-default">
+		<tr>
+			<th scope="col" class="px-6 py-3 font-medium">
+				ID
+			</th>
+			<th scope="col" class="px-6 py-3 font-medium">
+				NOMBRE
+			</th>
+            <th scope="col" class="px-6 py-3 font-medium">
+                COMERCIAL
+            </th>
+            <th scope="col" class="px-6 py-3 font-medium">
+                TIPO
+            </th>
+            <th scope="col" class="px-6 py-3 font-medium">
+                PROVINCIA
+            </th>
+			<th scope="col" class="px-6 py-3 font-medium" width="10px">
+				ACTIONS
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+
+		@foreach ($clientes as $cliente)
+		<tr class="bg-neutral-primary border-b border-default">
+			<th scope="row" class="px-6 py-4 font-medium text-heading whitespace-nowrap">
+				{{ $cliente->id }}
+			</th>
+			<td class="px-6 py-4">
+				{{ $cliente->name }}
+			</td>
+            <td class="px-6 py-4">
+				{{ $cliente->vendedor->name }}
+			</td>
+            <td class="px-6 py-4">
+				{{ $cliente->tipo->label()  }}
+			</td>
+            <td class="px-6 py-4">
+				{{ $cliente->provincia->name }}
+			</td>
+			<td class="px-6 py-4 grid grid-cols-2">
+                <a href="{{ route('clientes.edit', ['cliente' => $cliente]) }}" class="inline-flex items-center gap-2 text-blue-600 hover:text-blue-800"">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                    </svg>
+                </a>
+                <form class="delete-form" action="{{ route('clientes.destroy', $cliente->id) }}" method="POST">
+
+					@csrf
+
+					@method('DELETE')
+
+					<button type="submit" class="inline-flex items-center gap-2 text-red-600 hover:text-red-800 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                        </svg>
+                    </button>
+				</form>
+                <a href="{{ route('clientes.show', ['cliente' => $cliente]) }}" class="inline-flex items-center gap-2 text-black hover:text-gray-700">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+                </a>
+            </td>
+		</tr>
+
+		@endforeach
+	</tbody>
+    </table>
+</div>
+
+<div class="mt-4">
+	{{ $clientes->links() }}
+</div>
+
+@push('js')
+	<script>
+		let forms = document.querySelectorAll('.delete-form');
+
+		forms.forEach(form => {
+			form.addEventListener('submit', (e)=>{
+				e.preventDefault();
+
+				Swal.fire({
+					title: "Quieres eliminar este cliente?",
+					text: "Este cambio no es reversible",
+					icon: "warning",
+					showCancelButton: true,
+					confirmButtonColor: "#3085d6",
+					cancelButtonColor: "#d33",
+					confirmButtonText: "Si, eliminalo"
+					}).then((result) => {
+					if (result.isConfirmed) {
+						form.submit();
+					}
+				});
+
+			});
+		});
+	</script>
+@endpush
+</x-layouts::app>
+
+
+@endif
