d94d603abf39b37653ad06e429bc673e8990a208
David Soto <david.soto@softeca.es>
2026-03-13 13:59:18 +0100

    feat: complete functions CRUD

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Enums\TipoCliente;
 use App\Models\Cliente;
+use App\Models\Producto;
+use App\Models\Provincia;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\Enum;
 
@@ -30,7 +33,11 @@ class ClienteController extends Controller
      */
     public function create()
     {
-        return view('clientes.create');
+        $productos = Producto::all();
+        $vendedores = User::vendedores()->get();
+        $provincias = Provincia::orderBy('name')->get();
+
+        return view('clientes.create', compact('productos', 'vendedores', 'provincias'));
     }
 
     /**
@@ -40,15 +47,24 @@ class ClienteController extends Controller
     {
         $data = $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|email|unique:clientes',
+            'email' => 'required|email|unique:clientes,email',
             'phone' => 'required',
             'zipcode' => 'nullable',
             'provincia_id' => 'required|exists:provincias,id',
-            'vendedor_id' => 'required|exists:vendedores,id',
+            'vendedor_id' => auth()->user()->hasRole('comercial') ? 'nullable' : 'required|exists:users,id',
             'tipo' => [new Enum(TipoCliente::class)],
+            'productos' => 'nullable|array',
+            'productos.*' => 'exists:productos,id',
         ]);
 
-        Cliente::create($data);
+        if (auth()->user()->hasRole('comercial'))
+        {
+            $data['vendedor_id'] = auth()->id();
+        }
+
+
+        $cliente = Cliente::create($data);
+        $cliente->productos()->sync($data['productos'] ?? []);
 
         session()->flash('swal',[
             'icon' => 'success',
@@ -72,7 +88,11 @@ class ClienteController extends Controller
      */
     public function edit(Cliente $cliente)
     {
-        return view('clientes.edit', ['cliente' => $cliente]);
+        $productos = Producto::all();
+        $vendedores = User::vendedores()->get();
+        $provincias = Provincia ::orderBy('name')->get();
+
+        return view('clientes.edit', ['cliente' => $cliente, 'productos' => $productos, 'vendedores' => $vendedores, 'provincias' => $provincias,]);
     }
 
     /**
@@ -86,12 +106,16 @@ class ClienteController extends Controller
             'phone' => 'required',
             'zipcode' => 'nullable',
             'provincia_id' => 'required|exists:provincias,id',
-            'vendedor_id' => 'required|exists:vendedores,id',
+            'vendedor_id' => auth()->user()->hasRole('comercial') ? 'nullable' : 'required|exists:users,id',
             'tipo' => [new Enum(TipoCliente::class)],
+            'productos' => 'nullable|array',
+            'productos.*' => 'exists:productos,id',
         ]);
 
 
         $cliente->update($data);
+
+        $cliente->productos()->sync($data['productos'] ?? []);
 
         session()->flash('swal',[
             'icon' => 'info',
