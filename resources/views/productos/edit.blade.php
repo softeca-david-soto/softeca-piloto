e3273006abdbf3895da2ab6ff6d5fda33dabbd76
David Soto <david.soto@softeca.es>
2026-03-16 10:42:27 +0100

    feat: add products views

--- resources/views/productos/edit.blade.php
+++ resources/views/productos/edit.blade.php
@@ -1,16 +1,17 @@
-<x-layouts::app title="Nuevo Producto">
+<x-layouts::app title="Editar Producto">
     <flux:breadcrumbs class="mb-4">
         <flux:breadcrumbs.item href="{{ route('dashboard') }}">Dashboard</flux:breadcrumbs.item>
         <flux:breadcrumbs.item href="{{ route('productos.index') }}">Productos</flux:breadcrumbs.item>
-        <flux:breadcrumbs.item>Crear</flux:breadcrumbs.item>
+        <flux:breadcrumbs.item>Editar</flux:breadcrumbs.item>
     </flux:breadcrumbs>
 
-    <form action="{{ route('productos.store') }}" method="POST" class="space-y-4">
+    <form action="{{ route('productos.update', $producto) }}" method="POST" class="space-y-4">
         @csrf
+        @method('PUT')
         <div class="space-y-4">
-            <flux:input name="name" placeholder="Aceite de Oliva" label="Nombre" value="{{ old('name') }}"></flux:input>
-            <flux:input name="reference" placeholder="00AA" label="Referencia" value="{{ old('reference') }}"></flux:input>
-            <flux:input name="stock" placeholder="0" label="Stock" type="number" value="{{ old('stock', 0) }}"></flux:input>
+            <flux:input name="name" placeholder="Aceite de Oliva" label="Nombre" value="{{ old('name', $producto->name) }}"></flux:input>
+            <flux:input name="reference" placeholder="REF-001" label="Referencia" value="{{ old('reference', $producto->reference) }}"></flux:input>
+            <flux:input name="stock" placeholder="0" label="Stock" type="number" value="{{ old('stock', $producto->stock) }}"></flux:input>
 
             <div class="border border-default rounded-base p-4 space-y-4">
                 <p class="text-sm font-medium text-body uppercase tracking-widest">Precios por tipo de cliente</p>
@@ -21,13 +22,13 @@
                     placeholder="0.00"
                     type="number"
                     step="0.01"
-                    value="{{ old('precio_' . $tipo->value) }}">
+                    value="{{ old('precio_' . $tipo->value, $producto->{'precio_' . $tipo->value}) }}">
                 </flux:input>
                 @endforeach
             </div>
 
             <div class="flex justify-end mt-4">
-                <flux:button type="submit" variant="primary">Crear Producto</flux:button>
+                <flux:button type="submit" variant="primary">Guardar Cambios</flux:button>
             </div>
         </div>
     </form>
