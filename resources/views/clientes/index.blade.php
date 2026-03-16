0287fab1be19d0543049b310e9fc2813472060d2
David Soto <david.soto@softeca.es>
2026-03-16 08:04:37 +0100

    feat: give gray backgroud to thead

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -43,7 +43,7 @@
 
 	<div class="relative overflow-x-auto bg-neutral-primary-soft shadow-xs rounded-base border border-default mb-4">
     <table class="w-full text-sm text-left rtl:text-right text-body">
-	<thead class="text-sm text-body bg-neutral-secondary-soft border-b rounded-base border-default">
+	<thead class="text-sm text-body bg-gray-200 border-b rounded-base border-default">
 		<tr>
 			<th scope="col" class="px-6 py-3 font-medium">
 				ID
