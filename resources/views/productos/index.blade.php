936a183fee73343ed9c5e0b65b056031b01bae0b
David Soto <david.soto@softeca.es>
2026-03-18 12:25:08 +0100

    fix: fix delete confirmations

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -84,15 +84,28 @@
 
     @push('js')
         <script>
-            let forms = document.querySelectorAll('.delete-form');
-            forms.forEach(form => {
-                form.addEventListener('submit', (e) => {
-                    e.preventDefault();
-                    if (confirm("¿Quieres eliminar este producto? Este cambio no es reversible")) {
-                        form.submit();
-                    }
-                });
-            });
-        </script>
+		let forms = document.querySelectorAll('.delete-form');
+
+		forms.forEach(form => {
+			form.addEventListener('submit', (e)=>{
+				e.preventDefault();
+
+				Swal.fire({
+					title: "Quieres eliminar este producto?",
+					text: "Este cambio no es reversible",
+					icon: "warning",
+					showCancelButton: true,
+					confirmButtonColor: "#3085d6",
+					cancelButtonColor: "#d33",
+					confirmButtonText: "Si, eliminalo"
+					}).then((result) => {
+					if (result.isConfirmed) {
+						form.submit();
+					}
+				});
+
+			});
+		});
+	</script>
     @endpush
 </x-layouts::app>
