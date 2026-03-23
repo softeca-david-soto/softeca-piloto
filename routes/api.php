fd0b59db65901aecfdbcaec0b714fec3c9b9c4b3
David Soto <david.soto@softeca.es>
2026-03-23 08:16:44 +0100

    fix: fix route conflict

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\ClienteApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'clientes', 'as' => 'apiclientes.', 'controller' => ClienteApiController::class], function (){
+
+    Route::get('/', 'index')->name('index');
+});
